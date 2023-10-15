12520aaf925f3eeed325fbbe827d7b259841f95d
sarukiti <sarukiti1891@gmail.com>
2023-10-16 02:36:57 +0900

    屁理屈の粋

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use drobo_interfaces::srv::{SolenoidStateSrv, SolenoidStateSrv_Request};
 use gpio_cdev::{Chip, LineRequestFlags};
-use safe_drive::{context::Context, error::DynError};
+use safe_drive::{context::Context, error::DynError, qos::Profile, service::client::Client};
+use tokio::time::timeout;
 use vl53l1x;
 
+use core::panic;
 use std::time::Duration;
 
 #[tokio::main]
@@ -26,6 +29,7 @@ async fn main() -> Result<(), DynError> {
     vl_rear.soft_reset()?;
     vl_rear.init()?;
     vl_rear.set_device_address(0x31)?;
+    vl_rear.init()?;
     vl_rear.start_ranging(vl53l1x::DistanceMode::Mid)?;
 
     handle_mid_sensor.set_value(1)?;
@@ -33,6 +37,7 @@ async fn main() -> Result<(), DynError> {
     vl_mid.soft_reset()?;
     vl_mid.init()?;
     vl_mid.set_device_address(0x30)?;
+    vl_mid.init()?;
     vl_mid.start_ranging(vl53l1x::DistanceMode::Mid)?;
 
     handle_front_sensor.set_value(1)?;
@@ -43,44 +48,46 @@ async fn main() -> Result<(), DynError> {
 
     let ctx = Context::new()?;
     let node = ctx.create_node("pole_detector", None, Default::default())?;
-    let publisher =
-        node.create_publisher::<drobo_interfaces::msg::SolenoidStateMsg>("solenoid_order", None)?;
-    let mut msg = drobo_interfaces::msg::SolenoidStateMsg::new().unwrap();
-    
+    let mut client = node.create_client::<drobo_interfaces::srv::SolenoidStateSrv>("solenoid_order", Some(Profile::default()))?;
+    let mut msg = drobo_interfaces::srv::SolenoidStateSrv_Request::new().unwrap();    
 
     loop {
         let s_front = vl_front.read_sample()?.distance;
-        if s_front < 1100 {
-            // サポート機構を上げる
-            let support_address = 0x04;
-            msg.axle_position = support_address;
-            msg.state = true;
-            publisher.send(&msg)?;
+        let s_middle = vl_mid.read_sample()?.distance;
+        let s_rear = vl_rear.read_sample()?.distance;
 
+        if s_front < 1100 {
             // ここからタイヤの上げ下げ
             msg.axle_position = 0;
-            msg.state = true;
-            publisher.send(&msg)?;
-
-            while vl_mid.read_sample()?.distance < 1100 {}
-            msg.state = false;
-            publisher.send(&msg)?;
-            msg.axle_position = 1;
-            msg.state = true;
-            publisher.send(&msg)?;
-
-            while vl_rear.read_sample()?.distance < 1100 {}
-            msg.state = false;
-            publisher.send(&msg)?;
-            msg.axle_position = 2;
-            msg.state = true;
-            publisher.send(&msg)?;
-
-            std::thread::sleep(Duration::from_secs(1));
-            msg.state = false;
-            publisher.send(&msg)?;
-        } else {
-            std::thread::sleep(Duration::from_millis(10));
+            msg.state = 1;
+            client = send_solenoid_state_srv(client, &msg).await;
         }
+        if s_middle < 1100 {
+            msg.axle_position = 0;
+            msg.state = 0;
+            client = send_solenoid_state_srv(client, &msg).await;
+            msg.axle_position = 1;
+            msg.state = 1;
+            client = send_solenoid_state_srv(client, &msg).await;
+        }
+        if s_rear < 1100 {
+            msg.axle_position = 1;
+            msg.state = 0;
+            client = send_solenoid_state_srv(client, &msg).await;
+            msg.axle_position = 2;
+            msg.state = 1;
+            client = send_solenoid_state_srv(client, &msg).await;
+        }
+        std::thread::sleep(Duration::from_millis(10));
+    }
+}
+
+async fn send_solenoid_state_srv(client: Client<SolenoidStateSrv>, msg: &SolenoidStateSrv_Request) -> Client<SolenoidStateSrv>{
+    let client_rcv = client.send(&msg).unwrap();
+    let mut receiver = client_rcv.recv();
+    match timeout(Duration::from_secs(1), &mut receiver).await {
+        Ok(Ok((c, response, _header))) => c,
+        Ok(Err(e)) => panic!(),
+        Err(elapsed) => receiver.give_up(),
     }
 }
